990538750faa21ac8f43cae1686514950ee5b784
Birkin James Diana <birkin.diana@gmail.com>
2023-03-05 21:32:52 -0500

    improves logging.

--- src/main.rs
+++ src/main.rs
@@ -17,9 +17,14 @@ Rust semaphore and mutex example.
 #[macro_use]
 extern crate log;
 
-use env_logger::{Builder, Target};
+// use env_logger::{Builder, Target};
+// use env_logger;
+use env_logger::{Builder};
+// use log::{error, warn, info, debug, trace};
+// use log::{LevelFilter, Metadata, Record};
+use log::{LevelFilter};
 use rand::Rng;
-
+use std::io::Write;
 
 
 // set up envars ----------------------------------------------------
@@ -30,14 +35,42 @@ use rand::Rng;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // set up logging ---------------------------------------------------
-    let mut log_builder = Builder::from_default_env();
-    log_builder.target(Target::Stdout);
-    log_builder.init();
-    let msg: String = format!(
-        "\n\n-------\n`starting logchecker_project code at, ``{:?}``",
-        local_time.to_rfc3339()
-    );
-    info!("{}", msg);
+    let mut builder = Builder::new();
+    builder.filter(None, LevelFilter::Info);
+
+    builder.format(|buf, record| {
+        let mut level_style = buf.style();
+        level_style.set_bold(true);
+        // level_style.set_color(Some(level_color(record.level())));
+
+        let mut target_style = buf.style();
+        target_style.set_bold(true);
+
+        writeln!(
+            buf,
+            "{} {} [{}] {}:{}: {}",
+            chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+            level_style.value(record.level()),
+            target_style.value(record.target()),
+            record.file().unwrap_or("unknown"),
+            record.line().unwrap_or(0),
+            record.args()
+        )
+    });
+
+    builder.init();
+
+    debug!("debug message");
+    info!("info message");
+
+    // let mut log_builder = Builder::from_default_env();
+    // log_builder.target(Target::Stdout);
+    // log_builder.init();
+    // let msg: String = format!(
+    //     "\n\n-------\n`starting logchecker_project code at, ``{:?}``",
+    //     local_time.to_rfc3339()
+    // );
+    // info!("{}", msg);
 
     let urls: Vec<std::string::String> = make_urls().await;
     println!("urls, ``{:#?}``", urls);
