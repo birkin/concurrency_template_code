d5aed6514ead3b453fe1b2db1e364c1f7f336bc6
Birkin James Diana <birkin.diana@gmail.com>
2023-03-06 21:47:32 -0500

    cleans up log-config.

--- src/main.rs
+++ src/main.rs
@@ -2,57 +2,35 @@
 Rust semaphore and mutex example.
 */
 
-// use std::env;
-// use std::fs::File;
-// use std::io::prelude::*;
-// use std::sync::Arc;
-
-// use tokio::sync::mpsc;
-// use tokio::sync::Semaphore;
-// use tokio::task;
-// use serde::{Deserialize, Serialize};
-
 // set up imports ---------------------------------------------------
-
 #[macro_use]
 extern crate log;
 
-// use env_logger::{Builder, Target};
-// use env_logger;
 use env_logger::{Builder};
-// use log::{error, warn, info, debug, trace};
-// use log::{LevelFilter, Metadata, Record};
 use log::{LevelFilter};
 use rand::Rng;
 use std::io::Write;
-
-
-// set up envars ----------------------------------------------------
-
 
 // main controller --------------------------------------------------
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    println!( "starting main()" );
-    // set up logging ---------------------------------------------------
-    // let log_level = std::env::var("API_KEY")?;
-    let log_level = std::env::var("CNCRNCY_TMPLT__LOG_LEVEL").unwrap_or_else(|error| {
+    // set up logging -----------------------------------------------
+    let mut builder = Builder::new();
+    let log_level: String = std::env::var("CNCRNCY_TMPLT__LOG_LEVEL").unwrap_or_else(|error| {
         panic!("Problem getting envar -- ``{:?}``", error);
     });
-    println!("log_level, ``{}``", log_level);
-
-    let mut builder = Builder::new();
-    builder.filter(None, LevelFilter::Info);
-
+    if log_level == "debug" {
+        builder.filter(None, LevelFilter::Debug);
+    } else  {
+        builder.filter(None, LevelFilter::Info);
+    }
     builder.format(|buf, record| {
         let mut level_style = buf.style();
         level_style.set_bold(true);
         // level_style.set_color(Some(level_color(record.level())));
-
         let mut target_style = buf.style();
         target_style.set_bold(true);
-
         writeln!(
             buf,
             "{} {} [{}] {}:{}: {}",
@@ -64,21 +42,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             record.args()
         )
     });
-
     builder.init();
+    debug!("logging initialized");
 
-    debug!("debug message");
-    info!("info message");
-
-    // let mut log_builder = Builder::from_default_env();
-    // log_builder.target(Target::Stdout);
-    // log_builder.init();
-    // let msg: String = format!(
-    //     "\n\n-------\n`starting logchecker_project code at, ``{:?}``",
-    //     local_time.to_rfc3339()
-    // );
-    // info!("{}", msg);
-
+    // make urls ----------------------------------------------------
     let urls: Vec<std::string::String> = make_urls().await;
     println!("urls, ``{:#?}``", urls);
  
