a49c5f07fe6a075ee8b7f038b710ff8deec6331e
Birkin James Diana <birkin.diana@gmail.com>
2023-03-10 22:48:29 -0500

    adds shared-process storage.

--- trio_sem_mutex.py
+++ trio_sem_mutex.py
@@ -6,6 +6,7 @@ Note: technically uses trio's `CapacityLimiter` as a semaphore, because it's eas
 import datetime
 import json
 import logging
+import math
 import os
 import pprint
 import random
@@ -30,14 +31,24 @@ LIMIT = int( os.environ.get('ASYNC_PY_TEST__LIMIT', '3') )              # permis
 log.debug( f'LIMIT, ``{LIMIT}``' )
 RESULTS_FILE_PATH = os.environ.get('ASYNC_PY_TEST__RESULTS_FILE_PATH', 'results.json')
 log.debug( f'RESULTS_FILE_PATH, ``{RESULTS_FILE_PATH}``' )
+SYNCHRONOUS_WRITE_DELAY = float( os.environ.get('ASYNC_PY_TEST__SYNCHRONOUS_WRITE_DELAY', '0.5') )
+log.debug( f'SYNCHRONOUS_WRITE_DELAY, ``{SYNCHRONOUS_WRITE_DELAY}``' )
 
-
-log.debug( '''
+estimate_sequential_processing_time = math.ceil( (URLS_COUNT * 2) + (URLS_COUNT * SYNCHRONOUS_WRITE_DELAY) )
+estimate_async_processing_time_a = math.ceil( (URLS_COUNT * 2 / LIMIT) )
+# log.debug( f'estimate_async_processing_time_a, ``{estimate_async_processing_time_a}``' )
+estimate_async_processing_time_b = math.ceil( (URLS_COUNT * SYNCHRONOUS_WRITE_DELAY) )
+# log.debug( f'estimate_async_processing_time_b, ``{estimate_async_processing_time_b}``' )
+estimate_async_processing_time = estimate_async_processing_time_a + estimate_async_processing_time_b
+# log.debug( f'estimate_async_processing_time, ``{estimate_async_processing_time}``' )
+log.debug( f'''
 ---
-For 10 urls, and a limit of 3 requests at-a-time, 
-  we should expect 4 sets of calls, with each call and thus each set taking about 2 seconds. 
-With a little extra time for overhead, and sequential json-file writes,
-  the total time should be a little over 8 seconds.
+For {URLS_COUNT} urls, each requiring about 2-seconds to respond, 
+    and a file-write delay of {SYNCHRONOUS_WRITE_DELAY} seconds, 
+    sequential processing would take about {estimate_sequential_processing_time} seconds.
+
+Our asynchrous processing, with a limit of {LIMIT} requests at-a-time, 
+    should take a little over {estimate_async_processing_time} seconds.
 ---
 ''' )
 
@@ -46,6 +57,7 @@ async def main():
     """ Controller:
         Sets up urls & loads up queue of urls with call to fetch().
         Called by if __name__ == '__main__' """
+    
     ## set up urls --------------------------------------------------
     url_data = []
     total_delay = 0
@@ -62,15 +74,25 @@ async def main():
     with open('results.json', 'w') as f:
         jsn = json.dumps( {'run_timestamp': str(datetime.datetime.now())} )
         f.writelines( [jsn, '\n'],  )
+
     ## query urls ---------------------------------------------------
     pre_start_time = time.monotonic()
+    results_dct = {}                            # fetch() will update this dict, just to show how data from different tasks can be shared
     limiter = trio.CapacityLimiter( LIMIT )     # trio.Semaphore( LIMIT ) would also work, but CapacityLimiter is easier to inspect
     lock = trio.Lock()
-    async with trio.open_nursery() as nursery:  # if I wanted to store results, I could define a result-dict just before or after this line, and pass it to the nursery
+    async with trio.open_nursery() as nursery:
         for entry in url_data:
             url = entry['url']
             job_name = entry['job_name']
-            nursery.start_soon(fetch, url, job_name, limiter, lock)
+            nursery.start_soon(fetch, url, job_name, results_dct, limiter, lock)
+
+    ## final write to json file ------------------------------------
+    with open('results.json', 'a') as f:
+        jsn = json.dumps( results_dct, sort_keys=True, indent=2 )
+        f.writelines( [jsn, '\n'] )
+    log.debug( f'results_dct appended to json file' )
+
+    ## log time ----------------------------------------------------
     post_end_time = time.monotonic()            # hit after all the nursery tasks are done
     full_elapsed_time = post_end_time - pre_start_time
     log.debug( f'full_elapsed_time, ``{full_elapsed_time}``' )    
@@ -78,7 +100,7 @@ async def main():
     return
 
 
-async def fetch(url, job_name, limiter, lock):
+async def fetch(url, job_name, results_dct, limiter, lock):
     """ Queries urls asynchronously (respecting limiter), and updates (synchronously) json file.
         Called by main() """
     async with limiter:
@@ -88,8 +110,13 @@ async def fetch(url, job_name, limiter, lock):
         log.debug( f'job, ``{job_name}`` get starting' )
         async with httpx.AsyncClient() as client:
             response = await client.get(url)
-            assert response.status_code == 200
+            assert response.status_code == 200, response.status_code
             data = response.text
+            jdct = json.loads( data )
+            # log.debug( f'jdct, ``{pprint.pformat(jdct)}``' )
+            trace_id = jdct['headers']['X-Amzn-Trace-Id']
+            results_dct[ job_name ] = trace_id
+            log.debug( f'trace_id, ``{trace_id}``' )
             log.debug( f'job, ``{job_name}`` response received' )
         end_time = time.monotonic()
         elapsed_time = end_time - start_time
@@ -105,7 +132,7 @@ async def fetch(url, job_name, limiter, lock):
 def update_json_file( url, elapsed_time, job_name ):
     """ Updates file of json lines.
         Called by fetch(), synchronously. """
-    # time.sleep( 0.5 )  ## enable this and view the logs to better see the effect of the lock
+    time.sleep( SYNCHRONOUS_WRITE_DELAY )  ## set this to .5 and view the logs to better see the effect of the lock
     with open('results.json', 'a') as f:
         jsn = json.dumps( {'url': url, 'time-taken': elapsed_time} )
         f.writelines( [jsn, '\n'] )
@@ -127,107 +154,21 @@ Usage...
 """
 Sample output...
 
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-<module>()::28] URLS_COUNT, ``10``
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-<module>()::30] LIMIT, ``3``
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-<module>()::32] RESULTS_FILE_PATH, ``results.json``
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-<module>()::35] 
+[10/Mar/2023 22:43:10] DEBUG [trio_sem_mutex-<module>()::29] URLS_COUNT, ``10``
+[10/Mar/2023 22:43:10] DEBUG [trio_sem_mutex-<module>()::31] LIMIT, ``3``
+[10/Mar/2023 22:43:10] DEBUG [trio_sem_mutex-<module>()::33] RESULTS_FILE_PATH, ``results.json``
+[10/Mar/2023 22:43:10] DEBUG [trio_sem_mutex-<module>()::35] SYNCHRONOUS_WRITE_DELAY, ``0.5``
+[10/Mar/2023 22:43:10] DEBUG [trio_sem_mutex-<module>()::44] 
 ---
-For 10 urls, and a limit of 3 requests at-a-time, 
-  we should expect 4 sets of calls, with each call and thus each set taking about 2 seconds. 
-With a little extra time for overhead, and sequential json-file writes,
-  the total time should be a little over 8 seconds.
----
+For 10 urls, each requiring about 2-seconds to respond, 
+    and a file-write delay of 0.5 seconds, 
+    sequential processing would take about 25 seconds.
 
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-main()::58] total_delay, ``19.856`` seconds
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-main()::59] url_data, ``[{'job_name': '2184', 'url': 'http://httpbin.org/delay/2.184'},
- {'job_name': '2169', 'url': 'http://httpbin.org/delay/2.169'},
- {'job_name': '2033', 'url': 'http://httpbin.org/delay/2.033'},
- {'job_name': '2057', 'url': 'http://httpbin.org/delay/2.057'},
- {'job_name': '1909', 'url': 'http://httpbin.org/delay/1.909'},
- {'job_name': '2037', 'url': 'http://httpbin.org/delay/2.037'},
- {'job_name': '1842', 'url': 'http://httpbin.org/delay/1.842'},
- {'job_name': '1819', 'url': 'http://httpbin.org/delay/1.819'},
- {'job_name': '1943', 'url': 'http://httpbin.org/delay/1.943'},
- {'job_name': '1863', 'url': 'http://httpbin.org/delay/1.863'}]``
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-fetch()::86] limiter-queue, ``<trio.CapacityLimiter at 0x104086da0, 3/3 with 7 waiting>``
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-fetch()::88] job, ``2033`` get starting
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-fetch()::86] limiter-queue, ``<trio.CapacityLimiter at 0x104086da0, 3/3 with 7 waiting>``
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-fetch()::88] job, ``2169`` get starting
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-fetch()::86] limiter-queue, ``<trio.CapacityLimiter at 0x104086da0, 3/3 with 7 waiting>``
-[05/Mar/2023 10:13:13] DEBUG [trio_sem_mutex_b-fetch()::88] job, ``2184`` get starting
-[05/Mar/2023 10:13:15] DEBUG [_client-_send_single_request()::1734] HTTP Request: GET http://httpbin.org/delay/2.033 "HTTP/1.1 200 OK"
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::93] job, ``2033`` response received
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::96] job, ``2033``; elapsed_time, ``2.1644889579620212``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::98] ready to write job, ``2033`` to json file at ``808225.059076875``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::100] got lock for job, ``2033`` about to write to json file at ``808225.059165625``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-update_json_file()::112] job, ``2033`` written to json file
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::86] limiter-queue, ``<trio.CapacityLimiter at 0x104086da0, 3/3 with 6 waiting>``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::88] job, ``2057`` get starting
-[05/Mar/2023 10:13:15] DEBUG [_client-_send_single_request()::1734] HTTP Request: GET http://httpbin.org/delay/2.169 "HTTP/1.1 200 OK"
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::93] job, ``2169`` response received
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::96] job, ``2169``; elapsed_time, ``2.240068915998563``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::98] ready to write job, ``2169`` to json file at ``808225.142316541``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::100] got lock for job, ``2169`` about to write to json file at ``808225.142569833``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-update_json_file()::112] job, ``2169`` written to json file
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::86] limiter-queue, ``<trio.CapacityLimiter at 0x104086da0, 3/3 with 5 waiting>``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::88] job, ``1909`` get starting
-[05/Mar/2023 10:13:15] DEBUG [_client-_send_single_request()::1734] HTTP Request: GET http://httpbin.org/delay/2.184 "HTTP/1.1 200 OK"
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::93] job, ``2184`` response received
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::96] job, ``2184``; elapsed_time, ``2.2563744590152055``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::98] ready to write job, ``2184`` to json file at ``808225.162304583``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::100] got lock for job, ``2184`` about to write to json file at ``808225.162376916``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-update_json_file()::112] job, ``2184`` written to json file
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::86] limiter-queue, ``<trio.CapacityLimiter at 0x104086da0, 3/3 with 4 waiting>``
-[05/Mar/2023 10:13:15] DEBUG [trio_sem_mutex_b-fetch()::88] job, ``2037`` get starting
-[05/Mar/2023 10:13:17] DEBUG [_client-_send_single_request()::1734] HTTP Request: GET http://httpbin.org/delay/2.057 "HTTP/1.1 200 OK"
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::93] job, ``2057`` response received
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::96] job, ``2057``; elapsed_time, ``2.1472854999592528``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::98] ready to write job, ``2057`` to json file at ``808227.207467375``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::100] got lock for job, ``2057`` about to write to json file at ``808227.207582416``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-update_json_file()::112] job, ``2057`` written to json file
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::86] limiter-queue, ``<trio.CapacityLimiter at 0x104086da0, 3/3 with 3 waiting>``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::88] job, ``1842`` get starting
-[05/Mar/2023 10:13:17] DEBUG [_client-_send_single_request()::1734] HTTP Request: GET http://httpbin.org/delay/2.037 "HTTP/1.1 200 OK"
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::93] job, ``2037`` response received
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::96] job, ``2037``; elapsed_time, ``2.1024795840494335``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::98] ready to write job, ``2037`` to json file at ``808227.265386458``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::100] got lock for job, ``2037`` about to write to json file at ``808227.265457458``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-update_json_file()::112] job, ``2037`` written to json file
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::86] limiter-queue, ``<trio.CapacityLimiter at 0x104086da0, 3/3 with 2 waiting>``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::88] job, ``1819`` get starting
-[05/Mar/2023 10:13:17] DEBUG [_client-_send_single_request()::1734] HTTP Request: GET http://httpbin.org/delay/1.909 "HTTP/1.1 200 OK"
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::93] job, ``1909`` response received
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::96] job, ``1909``; elapsed_time, ``2.175334749976173``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::98] ready to write job, ``1909`` to json file at ``808227.319158875``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::100] got lock for job, ``1909`` about to write to json file at ``808227.319217708``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-update_json_file()::112] job, ``1909`` written to json file
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::86] limiter-queue, ``<trio.CapacityLimiter at 0x104086da0, 3/3 with 1 waiting>``
-[05/Mar/2023 10:13:17] DEBUG [trio_sem_mutex_b-fetch()::88] job, ``1943`` get starting
-[05/Mar/2023 10:13:19] DEBUG [_client-_send_single_request()::1734] HTTP Request: GET http://httpbin.org/delay/1.842 "HTTP/1.1 200 OK"
-[05/Mar/2023 10:13:19] DEBUG [_client-_send_single_request()::1734] HTTP Request: GET http://httpbin.org/delay/1.819 "HTTP/1.1 200 OK"
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::93] job, ``1842`` response received
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::93] job, ``1819`` response received
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::96] job, ``1842``; elapsed_time, ``1.9448090840596706``
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::98] ready to write job, ``1842`` to json file at ``808229.153258875``
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::96] job, ``1819``; elapsed_time, ``1.8873844160698354``
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::98] ready to write job, ``1819`` to json file at ``808229.153523458``  ## NOTE: shows a slight delay before it can get the lock to write.
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::100] got lock for job, ``1842`` about to write to json file at ``808229.153599208``
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-update_json_file()::112] job, ``1842`` written to json file
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::86] limiter-queue, ``<trio.CapacityLimiter at 0x104086da0, 3/3 with 0 waiting>``
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::88] job, ``1863`` get starting
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::100] got lock for job, ``1819`` about to write to json file at ``808229.164902458``
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-update_json_file()::112] job, ``1819`` written to json file
-[05/Mar/2023 10:13:19] DEBUG [_client-_send_single_request()::1734] HTTP Request: GET http://httpbin.org/delay/1.943 "HTTP/1.1 200 OK"
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::93] job, ``1943`` response received
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::96] job, ``1943``; elapsed_time, ``2.0138801670400426``
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::98] ready to write job, ``1943`` to json file at ``808229.333527791``
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-fetch()::100] got lock for job, ``1943`` about to write to json file at ``808229.333823625``
-[05/Mar/2023 10:13:19] DEBUG [trio_sem_mutex_b-update_json_file()::112] job, ``1943`` written to json file
-[05/Mar/2023 10:13:21] DEBUG [_client-_send_single_request()::1734] HTTP Request: GET http://httpbin.org/delay/1.863 "HTTP/1.1 200 OK"
-[05/Mar/2023 10:13:21] DEBUG [trio_sem_mutex_b-fetch()::93] job, ``1863`` response received
-[05/Mar/2023 10:13:21] DEBUG [trio_sem_mutex_b-fetch()::96] job, ``1863``; elapsed_time, ``2.045841416926123``
-[05/Mar/2023 10:13:21] DEBUG [trio_sem_mutex_b-fetch()::98] ready to write job, ``1863`` to json file at ``808231.200360125``
-[05/Mar/2023 10:13:21] DEBUG [trio_sem_mutex_b-fetch()::100] got lock for job, ``1863`` about to write to json file at ``808231.200675041``
-[05/Mar/2023 10:13:21] DEBUG [trio_sem_mutex_b-update_json_file()::112] job, ``1863`` written to json file
-[05/Mar/2023 10:13:21] DEBUG [trio_sem_mutex_b-main()::76] full_elapsed_time, ``8.307708999956958``
+Our asynchrous processing, with a limit of 3 requests at-a-time, 
+    should take a little over 12 seconds.
+---
+[SNIP]
+(Lots of output shows the flow of processing, and the capacity-limiter and mutex working.)
+[SNIP]
+[10/Mar/2023 22:43:23] DEBUG [trio_sem_mutex-main()::98] full_elapsed_time, ``13.402209874941036``
 """
