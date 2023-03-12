b20463d3281d19fdcfe48f890b80ce942032c21d
Birkin James Diana <birkin.diana@gmail.com>
2023-03-12 09:37:29 -0400

    builds urls with random-numbers.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 readme coming....
+
+next...
+- build out make_urls()
+
+
+
+Note: for this template, I'll really store everything to a dict with the job-name being the four-character repreentation of the httpbin delay, and storing the amazon-id -- with the write being a "backup".
--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,12 @@ Rust semaphore and mutex example.
 #[macro_use]
 extern crate log;
 
-use env_logger::{Builder};
-use log::{LevelFilter};
+
+// use env_logger::{Builder};
+// use log::{LevelFilter};
 use rand::Rng;
-use std::io::Write;
+
+// use std::io::Write;
 
 // import logging
 use concurrency_template_code::setup_logging;
@@ -20,7 +22,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // set up logging -----------------------------------------------
     setup_logging();
-
     debug!( "main() log message" );
 
     // make urls ----------------------------------------------------
@@ -32,53 +33,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 
-// // set up logging ---------------------------------------------------
-// fn setup_logging() {
-//     let mut builder = Builder::new();
-//     let log_level_envar: String = "CNCRNCY_TMPLT__LOG_LEVEL".to_string();
-//     let log_level: String = std::env::var(&log_level_envar).unwrap_or_else(|error| {
-//         panic!("Problem getting envar, ``{:?}``; error, ``{:?}``", &log_level_envar, error);
-//     });
-//     if log_level == "debug" {
-//         builder.filter(None, LevelFilter::Debug);
-//     } else  {
-//         builder.filter(None, LevelFilter::Info);
-//     }
-//     builder.format(|buf, record| {
-//         let mut level_style = buf.style();
-//         level_style.set_bold(true);
-//         // level_style.set_color(Some(level_color(record.level())));
-//         let mut target_style = buf.style();
-//         target_style.set_bold(true);
-//         writeln!(
-//             buf,
-//             "{} {} [{}] {}:{}: {}",
-//             chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
-//             level_style.value(record.level()),
-//             target_style.value(record.target()),
-//             record.file().unwrap_or("unknown"),
-//             record.line().unwrap_or(0),
-//             record.args()
-//         )
-//     });
-//     builder.init();
-//     debug!("logging initialized");}
-
-
 // make urls --------------------------------------------------------
 async fn make_urls() -> Vec<std::string::String> {
     let mut urls: Vec<String> = Vec::new();
-    urls.push("https://www.google.com".to_string());
-    urls.push("https://www.yahoo.com".to_string());
-
-    // make a random integer between 1800 and 2200, inclusive
-    let mut rng = rand::thread_rng();
-    let random_number = rng.gen_range(1800..=2200);
-    println!("Random number between 1800 and 2200: {}", random_number);
-
-    let arr: [String; 3] = ["a".to_string(), "b".to_string(), "c".to_string()];
-    println!("arr, ``{:#?}``", arr);
-
+    let mut range_generator = rand::thread_rng();
+    for _i in 0..10 {
+        let random_number = range_generator.gen_range(1800..=2200);
+        let random_number_string = random_number.to_string();
+        let url: String = format!("http://httpbin.org/delay/{}", &random_number_string);
+        urls.push(url);
+    }
+    
     // return urls 
     urls
 }
+
+// async fn make_urls() -> Vec<std::string::String> {
+//     let mut urls: Vec<String> = Vec::new();
+//     urls.push("https://www.google.com".to_string());
+//     urls.push("https://www.yahoo.com".to_string());
+
+//     // make a random integer between 1800 and 2200, inclusive
+//     let mut rng = rand::thread_rng();
+//     let random_number = rng.gen_range(1800..=2200);
+//     println!("Random number between 1800 and 2200: {}", random_number);
+
+//     let arr: [String; 3] = ["a".to_string(), "b".to_string(), "c".to_string()];
+//     println!("arr, ``{:#?}``", arr);
+
+//     // return urls 
+//     urls
+// }
