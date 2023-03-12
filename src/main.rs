8411022aa8a9ae991c595043b997d1b5afbb8f6e
Birkin James Diana <birkin.diana@gmail.com>
2023-03-12 11:16:50 -0400

    starts with list of random numbers.

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,19 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     setup_logging();
     debug!( "main() log message" );
 
+    // generate list of random numbers ------------------------------
+    /* These will be used for dict-keys and url-delays. */
+    let random_nums: Vec<i32> = make_random_nums().await;
+    println!("random_nums, ``{:#?}``", random_nums);
+
+    // initialize results dict --------------------------------------
+    let results: std::collections::HashMap<i32, String> = make_results_dict( &random_nums ).await;
+    // let mut results: std::collections::HashMap<i32, String> = std::collections::HashMap::new();
+    // for random_num in &random_nums {
+    //     results.insert(*random_num, "not yet".to_string());
+    // }
+    println!("results, ``{:#?}``", results);
+
     // make urls ----------------------------------------------------
     let urls: Vec<std::string::String> = make_urls().await;
     println!("urls, ``{:#?}``", urls);
@@ -33,17 +46,48 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 
+// make results dict ------------------------------------------------
+async fn make_results_dict( random_nums: &Vec<i32> ) -> std::collections::HashMap<i32, String> {
+    /* Creates a dict with random-numbers as keys and 'not yet' as values. */
+    let mut results: std::collections::HashMap<i32, String> = std::collections::HashMap::new();
+    for random_num in random_nums {
+        results.insert(*random_num, "init".to_string());
+    }
+    results
+}
+
+// make random numbers ----------------------------------------------
+async fn make_random_nums() -> Vec<i32> {
+    /* Creates a list of unique random numbers. */
+    let mut random_nums: Vec<i32> = Vec::new();
+    while random_nums.len() < 10 {
+        let mut range_generator = rand::thread_rng();
+        let random_number: i32 = range_generator.gen_range(1800..=2200);
+        if !random_nums.contains(&random_number) {
+            random_nums.push(random_number);
+        }
+    }
+    // sort list
+    random_nums.sort();
+    random_nums
+}
+
 // make urls --------------------------------------------------------
 async fn make_urls() -> Vec<std::string::String> {
+    
+    // create a list of random numbers ------------------------------
+
     let mut urls: Vec<String> = Vec::new();
     let mut range_generator = rand::thread_rng();
     for _i in 0..10 {
-        let random_number = range_generator.gen_range(1800..=2200);
-        let random_number_string = random_number.to_string();
+        let random_number: i32 = range_generator.gen_range(1800..=2200);
+        // let zz: () = random_number;  // yields `found integer`
+
+        let random_number_string: String = random_number.to_string();
         let url: String = format!("http://httpbin.org/delay/{}", &random_number_string);
         urls.push(url);
     }
-    
+
     // return urls 
     urls
 }
