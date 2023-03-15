6ae6785d9e04e6055f48a781ae1f82bd7cd0f952
Birkin James Diana <birkin.diana@gmail.com>
2023-03-15 07:57:58 -0400

    passes mut reference to results to make-urls.

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ Rust semaphore and mutex example.
 // main imports -----------------------------------------------------
 #[macro_use]  // for logging
 extern crate log;
+// use serde_json::{json};
 use std::collections::BTreeMap;
-use serde_json::{json};
+use std::collections::HashMap;
 
 // lib.rs imports ---------------------------------------------------
 use concurrency_template_code::setup_logging;
@@ -33,8 +34,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     debug!( "testing to see if i still have access to results, ``{:#?}``", &results );
 
     // populate results dict with urls ------------------------------
-    // add_urls_to_results( &mut results, &random_nums ).await;
-    // debug!( "testing to see if i still have access to results, ``{:#?}``", &results );
+    add_urls_to_results( &mut results ).await;
+    debug!( "testing to see if i still have access to results, ``{:#?}``", &results );
 
 
 
@@ -44,27 +45,42 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 
 // make urls --------------------------------------------------------
-async fn add_urls_to_results( results: &mut BTreeMap<i32, String>, random_nums: &Vec<i32> ) {
+async fn add_urls_to_results( results: &mut BTreeMap<i32, HashMap<std::string::String, std::string::String>> ) {
     
     // iterate through results-dict entries --------------------------
-    for integer_element in random_nums {
-        // let zz: () = integer_element; // yields: found `&i32`
-        let mut url_dict: std::collections::BTreeMap<String, String> = std::collections::BTreeMap::new();
-        let seconds_float: f32 = *integer_element as f32 / 1000.0;
-        let url_value: String = format!("http://httpbin.org/delay/{:.3 }", seconds_float);  // limits the number of decimal places to 3
-        debug!( "url_value, ``{:#?}``", &url_value);
-
-        url_dict.insert("url".to_string(), url_value);
-        println!("url_dict, ``{:#?}``", &url_dict);
-        
-        let json_string = json!(url_dict).to_string();
-        println!("json_string, ``{:#?}``", &json_string);
-        results.insert( *integer_element, json_string );
+    for (key, value) in results {
+        debug!( "key, ``{:#?}``", &key);
+        debug!( "value, ``{:#?}``", &value);
     }
 
-    // return urls 
-    // urls
 }
+
+
+
+
+
+// // make urls --------------------------------------------------------
+// async fn add_urls_to_results( results: &mut BTreeMap<i32, String>, random_nums: &Vec<i32> ) {
+    
+//     // iterate through results-dict entries --------------------------
+//     for integer_element in random_nums {
+//         // let zz: () = integer_element; // yields: found `&i32`
+//         let mut url_dict: std::collections::BTreeMap<String, String> = std::collections::BTreeMap::new();
+//         let seconds_float: f32 = *integer_element as f32 / 1000.0;
+//         let url_value: String = format!("http://httpbin.org/delay/{:.3 }", seconds_float);  // limits the number of decimal places to 3
+//         debug!( "url_value, ``{:#?}``", &url_value);
+
+//         url_dict.insert("url".to_string(), url_value);
+//         println!("url_dict, ``{:#?}``", &url_dict);
+        
+//         let json_string = json!(url_dict).to_string();
+//         println!("json_string, ``{:#?}``", &json_string);
+//         results.insert( *integer_element, json_string );
+//     }
+
+//     // return urls 
+//     // urls
+// }
 
 
 // // make urls --------------------------------------------------------
