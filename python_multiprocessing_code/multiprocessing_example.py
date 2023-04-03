86a602943010a640694eafc7cacd736cd3bd27e9
Birkin James Diana <birkin.diana@gmail.com>
2023-04-03 16:35:45 -0400

    cleans up city/temp references.

--- python_multiprocessing_code/multiprocessing_example.py
+++ python_multiprocessing_code/multiprocessing_example.py
@@ -5,47 +5,42 @@ import requests
 
 
 # Fictional weather API
-def fetch_temperature(city):
-    # response = requests.get(f'http://weather-api.com/temperature/{city}')
-    url = f'http://httpbin.org/delay/{city}'
+def fetch_url(delay):
+    # response = requests.get(f'http://weather-api.com/temperature/{delay}')
+    url = f'http://httpbin.org/delay/{delay}'
     response = requests.get( url )
     response_id = response.json()['headers']['X-Amzn-Trace-Id']
-    # return city, response.json()['temperature']
-    return city, response_id
+    return delay, response_id
 
 # Writing results synchronously to JSON file
-def store_result(shared_results, city_temp):
+def store_result(shared_results, response_result):
     print( 'saving to file' )
     with shared_results.lock:
         time.sleep( .5 )
-        shared_results.results[city_temp[0]] = city_temp[1]
+        shared_results.results[response_result[0]] = response_result[1]
         with open('results.json', 'w') as f:
             json.dump( dict(shared_results.results), f, indent=2 )  # dict() is needed to make the dict-proxy object JSON serializable
 
-def worker(city_results_tuple):
-    city, shared_results = city_results_tuple
-    temperature = fetch_temperature(city)
-    store_result(shared_results, temperature)
+def worker( delay_results_tuple ):  # delay_results_tuple is of type `list[ tuple[float, _Namespace] ]`
+    delay, shared_results = delay_results_tuple
+    response_result = fetch_url(delay)
+    store_result( shared_results, response_result )
 
 def main():
     print( 'starting main()' )
-    cities = [2.104, 1.981, 1.802, 2.073, 1.956, 2.127, 2.015, 1.933, 2.003, 2.11]
-
-    # Initialize results dictionary
-    # with open('results.json', 'w') as f:
-    #     json.dump({}, f)
+    delays = [2.104, 1.981, 1.802, 2.073, 1.956, 2.127, 2.015, 1.933, 2.003, 2.11]  # from manually running ``random.randint(1800, 2200) / 1000``
 
     # Load environmental variable
-    max_concurrent_calls = int(os.getenv('MAX_CONCURRENT_WEATHER_API_CALLS', 3))
+    max_concurrent_calls = int(os.getenv('MAX_CONCURRENT_CALLS', 3))
 
     manager = Manager()
     shared_results = manager.Namespace()
     shared_results.lock = manager.Lock()
     shared_results.results: 'DictProxy[Any, Any]' = manager.dict()  # type: ignore
-    cities_results_tuples = [(city, shared_results) for city in cities]
+    delays_results_tuples = [(delay, shared_results) for delay in delays]
 
     with Pool(processes=max_concurrent_calls) as pool:
-        for _ in pool.imap_unordered(worker, cities_results_tuples):
+        for _ in pool.imap_unordered(worker, delays_results_tuples):
             pass
 
     print( 'leaving main()' )
