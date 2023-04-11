2b232a4c58fba3c749c6915b9b32ac5a1d3fb0fb
Birkin James Diana <birkin.diana@gmail.com>
2023-04-10 22:47:21 -0400

    works w/o max-concurrent-calls.

--- rust_code/src/main.rs
+++ rust_code/src/main.rs
@@ -1,87 +1,108 @@
 use std::collections::BTreeMap;
 use std::env;
 use std::fs::File;
-use std::io::Write;
+use std::io::prelude::*;
 use std::sync::{Arc, Mutex};
-use std::time::Instant;
 
-use serde::Deserialize;
+use ordered_float::OrderedFloat;
+use serde::{Serialize, Serializer};
+use serde_json;
 use tokio::task;
-use tokio::task::JoinHandle;
-use tokio::time::Duration;
 
-#[derive(Deserialize)]
-struct HttpbinResponse {
-    headers: HttpbinHeaders,
+#[derive(Clone, Eq, Hash, PartialEq, PartialOrd, Ord)]
+pub struct SerializableOrderedFloat(OrderedFloat<f64>);
+
+// impl From<f64> for SerializableOrderedFloat {
+//     fn from(value: f64) -> Self {
+//         SerializableOrderedFloat(OrderedFloat(value))
+//     }
+// }
+
+impl From<OrderedFloat<f64>> for SerializableOrderedFloat {
+    fn from(value: OrderedFloat<f64>) -> Self {
+        SerializableOrderedFloat(value)
+    }
 }
 
-#[derive(Deserialize)]
-struct HttpbinHeaders {
-    #[serde(rename = "X-Amzn-Trace-Id")]
-    x_amzn_trace_id: String,
+// impl Serialize for SerializableOrderedFloat {
+//     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+//     where
+//         S: Serializer,
+//     {
+//         self.0 .0.serialize(serializer)
+//     }
+// }
+
+impl Serialize for SerializableOrderedFloat {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_str(&format!("{:.3}", self.0 .0))
+    }
 }
 
-async fn fetch_url(delay: f64) -> (f64, String) {
-    let url = format!("https://httpbin.org/delay/{}", delay);
-    let response = reqwest::get(&url).await.unwrap();
-    let response_json: HttpbinResponse = response.json().await.unwrap();
-    let response_id = response_json.headers.x_amzn_trace_id;
-    (delay, response_id)
-}
 
-async fn worker(delay: f64, shared_results: Arc<Mutex<BTreeMap<f64, String>>>) {
-    let response_result = fetch_url(delay).await;
-    store_result(shared_results, response_result).await;
-}
-
-async fn store_result(
-    shared_results: Arc<Mutex<BTreeMap<f64, String>>>,
-    response_result: (f64, String),
-) {
-    println!("Saving to file");
-    let mut guard = shared_results.lock().unwrap();
-    guard.insert(response_result.0, response_result.1);
-    let mut file = File::create("results.json").unwrap();
-    file.write_all(serde_json::to_string_pretty(&*guard).unwrap().as_bytes())
-        .unwrap();
+#[derive(Serialize)]
+struct SerializableOrderedFloatMap {
+    results: BTreeMap<SerializableOrderedFloat, String>,
 }
 
 #[tokio::main]
 async fn main() {
-    println!("Starting main()");
-    let delays = vec![
+    let delays = [
         2.104, 1.981, 1.802, 2.073, 1.956, 2.127, 2.015, 1.933, 2.003, 2.11,
     ];
-    let max_concurrent_calls = env::var("MAX_CONCURRENT_CALLS")
-        .unwrap_or("3".to_string())
-        .parse::<usize>()
+    let max_concurrent_calls: usize = env::var("MAX_CONCURRENT_CALLS")
+        .unwrap_or_else(|_| "3".to_string())
+        .parse()
         .unwrap();
-    let shared_results = Arc::new(Mutex::new(BTreeMap::new()));
 
-    let start_time = Instant::now();
+    let shared_results = Arc::new(Mutex::new(BTreeMap::<SerializableOrderedFloat, String>::new()));
 
-    let mut handles: Vec<JoinHandle<()>> = vec![];
+    let mut handles = vec![];
 
-    for delay in delays {
+    for &delay in delays.iter() {
         let shared_results_clone = Arc::clone(&shared_results);
-        let handle = task::spawn(async move { worker(delay, shared_results_clone).await });
+        let handle = task::spawn(async move {
+            let response_result = fetch_url(delay).await;
+            store_result(shared_results_clone, response_result).await;
+        });
         handles.push(handle);
-        if handles.len() == max_concurrent_calls {
-            for handle in handles.drain(..) {
-                handle.await.unwrap();
-            }
-        }
     }
 
     for handle in handles {
         handle.await.unwrap();
     }
 
-    let end_time = Instant::now();
-    let full_elapsed_time = end_time.duration_since(start_time);
-    println!(
-        "Full elapsed time: {}.{:03} seconds",
-        full_elapsed_time.as_secs(),
-        full_elapsed_time.subsec_millis()
-    );
+    let guard = shared_results.lock().unwrap();
+    let results_map = SerializableOrderedFloatMap {
+        results: guard.clone(),
+    };
+    let json = serde_json::to_string_pretty(&results_map).unwrap();
+    println!("{}", json);
+}
+
+async fn fetch_url(delay: f64) -> (OrderedFloat<f64>, String) {
+    let url = format!("https://httpbin.org/delay/{:.3}", delay);
+    let response = reqwest::get(&url).await.unwrap();
+    let json: serde_json::Value = response.json().await.unwrap();
+    let response_id = json["headers"]["X-Amzn-Trace-Id"].as_str().unwrap().to_string();
+    (OrderedFloat(delay), response_id)
+}
+
+async fn store_result(
+    shared_results: Arc<Mutex<BTreeMap<SerializableOrderedFloat, String>>>,
+    response_result: (OrderedFloat<f64>, String),
+) {
+    let mut guard = shared_results.lock().unwrap();
+    guard.insert(response_result.0.into(), response_result.1);
+    let results_map = SerializableOrderedFloatMap {
+        results: guard.clone(),
+    };
+    let json = serde_json::to_string_pretty(&results_map).unwrap();
+    drop(guard);
+
+    let mut file = File::create("results.json").unwrap();
+    file.write_all(json.as_bytes()).unwrap();
 }
